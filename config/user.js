b98d037b32eb41ea8a6aeacefe0c3e6a62340f69
Devan Carpenter <git@dvn.me>
2022-06-02 23:18:52 +0100

    chore: remove legacy vendor js

--- config/user.js
+++
@@ -1,4 +0,0 @@
-user_pref("security.csp.enable", false);
-user_pref("security.mixed_content.block_active_content", false);
-user_pref("javascript.options.dynamicImport", true);
-user_pref("dom.moduleScripts.enabled", true);
--- js/freezedry/freeze.js
+++
@@ -1,2 +0,0 @@
-html = freezeDry.default();
-//console.log(html);
